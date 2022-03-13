e675644626696d175408b0110627b7e5b6f0dfa0
姚晗 <yaohan@ti-net.com.cn>
2022-03-13 22:16:31 +0800

    注册中心-服务注册逻辑修正

--- registerCenter/src/main/java/com/wycaca/service/RegisterCenterService.java
+++ registerCenter/src/main/java/com/wycaca/service/RegisterCenterService.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutorService;
@@ -24,8 +25,14 @@ public class RegisterCenterService {
 
     private static final ExecutorService keepAliveExecutor = Executors.newFixedThreadPool(10, new NamedThreadPoolFactory("keep_alive"));
 
-    // 保存已注册的服务, 服务名: 服务路径
+    // 保存已注册的服务, <服务类路径, <服务注册url, 服务对象>>
+    // com.tinet.ctilink.agent.service.AgentOutcallScheduleTaskService
+    private static final ConcurrentMap<String, ConcurrentMap<String, RegisterService>> providerPathMap = new ConcurrentHashMap<>();
     private static final ConcurrentMap<String, RegisterService> providerMap = new ConcurrentHashMap<>();
+
+    private static final ConcurrentMap<String, Map<String, RegisterService>> consumerPathMap = new ConcurrentHashMap<>();
+    // consumer://10.10.6.145/com.tinet.ctilink.agent.service.AgentOutcallScheduleTaskService?application=cti-link-agent-gateway&application.version=0.0.1&category=consumers&check=false&default.check=false&default.version=0.0.1&dubbo=2.8.4&interface=com.tinet.ctilink.agent.service.AgentOutcallScheduleTaskService&methods=pauseTask,start,listAgentTask,pause&pid=30269&revision=0.0.1&side=consumer&timestamp=1646892725284
+    private static final ConcurrentMap<String, RegisterService> consumerMap = new ConcurrentHashMap<>();
 
     public RegisterResponse register(String url) {
         RegisterService registerService = null;
@@ -38,16 +45,28 @@ public class RegisterCenterService {
         // 区分提供者, 消费者
         // 如果是提供者加入注册服务
         if (SystemConst.PROVIDER.equals(registerService.getType())) {
-            providerMap.put(registerService.getName(), registerService);
-            logger.info("生产者服务: {}, 注册成功, class path: {}", registerService.getName(), registerService.getPath());
+            // 已存在, 则更新 心跳时间
+            if (providerPathMap.containsKey(registerService.getPath())) {
+                registerService = providerPathMap.get(registerService.getPath()).get(registerService.getRegisterUrl());
+                registerService.setLastPingTimestamp(System.currentTimeMillis());
+            }
+            providerMap.put(registerService.getRegisterUrl(), registerService);
+            providerPathMap.put(registerService.getPath(), providerMap);
+            logger.info("生产者服务: {}, 注册/心跳成功, class path: {}", registerService.getName(), registerService.getPath());
             return RegisterResponse.ok();
         }
         // 如果是消费者, 查找提供者
         else if (SystemConst.CONSUMER.equals(registerService.getType())) {
-            logger.info("消费者服务: {}, 注册成功, 可用生产者 path: {}", registerService.getName(), registerService.getPath());
             // 如果有对应的提供者, 返回socket连接信息, 使提供者和消费者直接建立连接
-            if (providerMap.containsKey(registerService.getName())) {
-                return RegisterResponse.ok(registerService.getIp() + ":" + registerService.getPort());
+            if (consumerPathMap.containsKey(registerService.getPath())) {
+                // 选择一个生产者, 目前直接find any, 随机?
+                // todo 负载均衡策略
+                RegisterService providerService = providerPathMap.get(registerService.getPath()).values().stream().findAny().get();
+                // 注册消费者
+                consumerMap.put(registerService.getRegisterUrl(), registerService);
+                consumerPathMap.put(registerService.getPath(), consumerMap);
+                logger.info("消费者服务: {}, 注册成功", registerService.getPath());
+                return RegisterResponse.ok(providerService.getIp() + ":" + providerService.getPort());
             } else {
                 // 没有, 直接返回错误信息
                 return RegisterResponse.fail(HttpStatus.INTERNAL_SERVER_ERROR.value(), "未找到可用生产者, " + registerService.getName());
@@ -66,7 +85,6 @@ public class RegisterCenterService {
             while (true) {
                 // 服务发现线程池
                 registerExecutor.execute(new ServiceRegisterTask(serverSocket.accept()));
-                // todo 心跳检测线程池
             }
         } finally {
             serverSocket.close();
